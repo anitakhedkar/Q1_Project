1e3f36d46087bab6b0d52b23124375fb4a5016c9
ANITA KHEDKAR <anita.khedkar@gmail.com>
2017-02-08 20:16:39 -0700

    toggle

--- index1.js
+++ index1.js
@@ -7,6 +7,7 @@
   var marker;
   var popup;
   var weatherMarkers;
+  var trafficLayer;
 
   // function reset(){
   //    for (var i = 0; i < markers.length ; i++){
@@ -18,6 +19,8 @@
   $(".js-reset").on('click',initMap);
 
   function initMap(){
+     document.getElementById("destinationAddress").value = "Destination";
+     document.getElementById("startAddress").value = "Start";
       var mapOptions = {
         center : new google.maps.LatLng(39.73, -104.99),
         zoom : 10
@@ -32,6 +35,7 @@
       directionsService = new google.maps.DirectionsService();
       directionsRenderer = new google.maps.DirectionsRenderer();
   }
+
   $(".js-directions-form").on('submit',function(event){
   event.preventDefault();
   });
@@ -44,17 +48,13 @@
   function findDirections(){
     var start = document.getElementById("startAddress").value;
     var destination = document.getElementById("destinationAddress").value;
+
     Promise.all([getGeoCode(start),getGeoCode(destination)])
       .then(getDirection);
   }
 
-  // function captureLocations(locations){
-  //   locationArray = locations;
-  // }
-
   function getDirection(locations){
     locationArray = locations;
-
     directionsRenderer.setMap(map);
     directionsService.route({'origin':locations[0],'destination':locations[1],'travelMode':google.maps.TravelMode.DRIVING},
     function(result,status){
@@ -89,36 +89,50 @@ function placeMarkerOnMap(location){
   map.setZoom(12);
   markers.push(marker);
 }
-//Something to do with traffic toggle button
+// //Something to do with traffic toggle button
 function findTraffic(){
-  var trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
+  if(trafficLayer == null){
+    trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+  }else if (trafficLayer.getMap() !== null ) {
+    trafficLayer.setMap(null);
+  }else if(trafficLayer.getMap() == null ){
+    trafficLayer.setMap(map);
+  }
 }
 
-
  function findWeather(){
-// //trying to get the previous markers to hide.
-//    for(var i = 0; i < markers.length; i++){
-//      markers[i].setMap(null);
-//      markers = [];
-//    }
-  for(i = 0; i < locationArray.length ; i++){
-    var URL = 'http://api.openweathermap.org/data/2.5/weather?'+
-    'lat='+locationArray[i].lat()+'&lon='+locationArray[i].lng() +
-    '&units=metric&APPID=32ba66921dd068c08810dbf7575a9488';
-    console.log("URL is " + URL);
-    map.data.loadGeoJson($.getJSON(URL, gotData));
 
+   if(weatherMarkers == null || weatherMarkers.length == 0 ){
+        for(i = 0; i < locationArray.length ; i++){
+          var URL = 'http://api.openweathermap.org/data/2.5/weather?'+
+          'lat='+locationArray[i].lat()+'&lon='+locationArray[i].lng() +
+          '&units=metric&APPID=32ba66921dd068c08810dbf7575a9488';
+          map.data.loadGeoJson($.getJSON(URL, gotData));
+        }
+   }
+   else if(weatherMarkers.length > 0){
+     console.log("calling toggleWeatherMap");
+     toggleWeatherMap();
+   }
+}
+
+function toggleWeatherMap(){
+  for(var i = 0; i < weatherMarkers.length ; i++){
+    if(weatherMarkers[i].getMap() == null){
+      weatherMarkers[i].setMap(map);
+    }else{
+      weatherMarkers[i].setMap(null);
+    }
   }
+
 }
 
 function gotData(data){
   var iconImgUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
   var marker = new google.maps.Marker({
      map:map,
-     origin: new google.maps.Point(0, 0),
      clickable: true,
-     anchor: new google.maps.Point(-17, 0),
      position: new google.maps.LatLng(data.coord.lat, data.coord.lon),
      icon: iconImgUrl, // null = default icon
      zIndex:99999999 //to get the icon on the forefront
@@ -130,7 +144,6 @@ function gotData(data){
     position: new google.maps.LatLng(data.coord.lat, data.coord.lon)
   }
   var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-  //infoWindow.open(map);
   google.maps.event.addListener(marker, 'click', function(){
     infoWindow.open(map, marker);
   })
